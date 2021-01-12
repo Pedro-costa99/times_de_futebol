c3137aa5bd23679e5e8a058d305052e19ce84939
Pedro-costa99 <53200356+Pedro-costa99@users.noreply.github.com>
2021-01-12 19:46:31 -0300

    Add files via upload

---
+++ times_futebol/lib/src/cadastro/cadastro_component.dart
@@ -0,0 +1,8 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'cadastro',
+  templateUrl: 'cadastro_component.html',
+  styleUrls: ['cadastro_component.css'],
+)
+class CadastroComponent {}
